5adc5f08f676cc023c3fbad109a040d161aa4987
Eduardo SantAna <eduardosantana1313@gmail.com>
2022-03-24 19:38:53 -0300

    master java proxy

--- java.proxy/src/main/java/br/com/edu/java/proxy/MainProxy.java
+++ java.proxy/src/main/java/br/com/edu/java/proxy/MainProxy.java
@@ -1,22 +1,33 @@
 package br.com.edu.java.proxy;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class MainProxy {
-	public static void main(String[] args) {
 
-		MyInterface proxyInstance = (MyInterface) Proxy.newProxyInstance(MainProxy.class.getClassLoader(),
-				new Class[] { MyInterface.class }, (proxy, method, methodArgs) -> {
-					if (method.getName().equals("doBla")) {
-						return 42;
-					} else if (method.getName().equals("put")) {
-						return 10;
-					} else {
-						throw new UnsupportedOperationException("Unsupported method: " + method.getName());
-					}
-				});
+	public static void main(final String[] args) {
 
+		final MyInterface proxyInstance = (MyInterface) Proxy.newProxyInstance(MainProxy.class.getClassLoader(),
+				new Class[] {
+					MyInterface.class
+				}, handler());
+
+		System.out.println("testing...");
 		System.out.println(proxyInstance.doBla());
+	}
+
+	private static InvocationHandler handler() {
+		return (proxy, method, methodArgs) -> {
+
+			if (method.getName().equals("doBla")) {
+				return 42;
+			} else if (method.getName().equals("put")) {
+				return 10;
+			} else {
+				throw new UnsupportedOperationException("Unsupported method: " + method.getName());
+			}
+
+		};
 	}
 
 }
