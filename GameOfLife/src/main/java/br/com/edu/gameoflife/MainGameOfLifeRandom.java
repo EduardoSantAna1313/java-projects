1567b3b00c9bb44c706e43e95ee2c1062925e617
Eduardo SantAna <eduardosantana1313@gmail.com>
2022-03-24 20:10:08 -0300

    master jogo da vida

--- GameOfLife/src/main/java/br/com/edu/gameoflife/MainGameOfLifeRandom.java
+++ GameOfLife/src/main/java/br/com/edu/gameoflife/MainGameOfLifeRandom.java
@@ -9,10 +9,10 @@ import br.com.edu.gameoflife.gui.Window;
  */
 public class MainGameOfLifeRandom {
 
-	private static final double LIFE_RATE = 0.7;
+	private static final double LIFE_RATE = 0.5;
 
 	public static void main(final String[] pArgs) throws Exception {
-		final JFrame window = new Window(500, 500, LIFE_RATE);
+		final JFrame window = new Window(250, 250, LIFE_RATE);
 
 		window.setVisible(true);
 	}
