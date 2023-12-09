c22b90eb082b8a424cfe385bf5f21b3c68f910de
Eduardo <eduardosantana1313@gmail.com>
2023-12-08 21:47:42 -0300

    Either catches

--- either/src/main/java/br/com/edu/Either.java
+++ either/src/main/java/br/com/edu/Either.java
@@ -1,30 +1,31 @@
 package br.com.edu;
 
-import java.util.Objects;
 import java.util.function.Supplier;
 
 public record Either <F, S> (F fail, S success) {
 
     public static <F,S> Either<F,S> success(final S success) {
-        Objects.requireNonNull(success);
         return new Either<>(null, success);
     }
 
     public static <F,S> Either<F,S> success(final Supplier<S> success) {
-        Objects.requireNonNull(success);
-        Objects.requireNonNull(success.get());
         return new Either<>(null, success.get());
     }
 
     public static <F,S> Either<F,S> fail(final F fail) {
-        Objects.requireNonNull(fail);
         return new Either<>(fail, null);
     }
 
     public static <F,S> Either<F,S> fail(final Supplier<F> fail) {
-        Objects.requireNonNull(fail);
-        Objects.requireNonNull(fail.get());
         return new Either<>(fail.get(), null);
+    }
+
+    public static <F extends Exception,S> Either<F,S> catches(final Supplier<S> success) {
+        try {
+            return Either.success(success.get());
+        } catch (final Exception error) {
+            return new Either(error , null);
+        }
     }
 
 }
--- either/src/main/java/br/com/edu/Main.java
+++ either/src/main/java/br/com/edu/Main.java
@@ -2,18 +2,24 @@ package br.com.edu;
 
 public class Main {
 
-    public static void main(String[] args) {
-        final var valueFail = parse("abc");
-        System.out.println(valueFail);
-
-        final var valueSuccess = parse("123");
-        System.out.println(valueSuccess);
+    public static void main(final String[] args) {
+        System.out.println("\nFail: " + parse("abc"));
+        System.out.println("Success: " + parse("123"));
+        System.out.println("Catches: " + Either.catches(() -> potentialThrowingCode(1)));
+        System.out.println("Catches: " + Either.catches(() -> potentialThrowingCode(2)));
     }
 
     private static Either<NumberFormatException, Integer> parse(final String s ) {
         if (s.matches("\\d*"))
             return Either.success(Integer.parseInt(s));
         return Either.fail(NumberFormatException::new);
+    }
+
+    private static String potentialThrowingCode(int n) {
+        if (n == 1) {
+            return "" + n;
+        }
+        throw new IllegalArgumentException();
     }
 
 }
